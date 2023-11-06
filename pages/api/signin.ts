9c79ad04451a3b05ef766b578379195c51caf1b3
Abraham Franklin (Chengshuai) Yang <34443372+integritynoble@users.noreply.github.com>
2023-11-06 10:29:26 -0800

    Update signin.ts

--- pages/api/signin.ts
+++ pages/api/signin.ts
@@ -1,9 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+    process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
 );
-
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).end(); // Method Not Allowed
