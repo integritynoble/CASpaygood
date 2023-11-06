e4bc7bbcc55a4f0962355c60a40b3f1caeb6b8fa
Abraham Franklin (Chengshuai) Yang <34443372+integritynoble@users.noreply.github.com>
2023-11-06 10:25:48 -0800

    Update signin.ts

--- pages/api/signin.ts
+++ pages/api/signin.ts
@@ -1,45 +1,30 @@
-// /app/api/signin.ts
-
 import { createClient } from '@supabase/supabase-js';
-import type { Database } from 'types_db';
-import { redirect } from 'next/navigation';
-import { NextApiRequest, NextApiResponse } from 'next';
-
-const supabase = createClient<Database>(
-  process.env.NEXT_PUBLIC_SUPABASE_URL || '',
-  process.env.SUPABASE_SERVICE_ROLE_KEY || ''
+const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
 
-/**
- * Handle user sign in with Supabase.
- * @param {string} email - The user's email.
- * @param {string} password - The user's password.
- * @returns {Promise<Object>} - Returns user, session or error object.
- */
-const handleSignIn = async (email: string, password: string) => {
-    const response = await supabase.auth.signInWithPassword({ email, password });
-    const user = response.data?.user;
-    const session = response.data?.session;
-    const error = response.error;
-  if (error) {
-    throw error;
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).end(); // Method Not Allowed
   }
-  return { user, session };
-};
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    console.log('Received request body:',  req.method, req.headers, req.body);
-    if (req.method === 'POST') {
-        try {
-            const { email, password } = req.body;
-            const { user, session } = await handleSignIn(email, password);
-            return res.status(200).json({ user, session });
-        } catch (error) {
-            if (error instanceof Error) {
-                return res.status(401).json({ error: error.message });
-            } else {
-                return res.status(401).json({ error: 'An unexpected error occurred.' });
-            }
-        }
-    } 
+  const { email, password, code } = req.body;
+
+  // Handle email/password sign-in
+  if (email && password) {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: email,
+      password: password
+      
+    })
+
+    if (error) {
+      return res.status(400).json({ error: error.message });
+    }
+
+    return res.status(200).json({ user: data.user, session: data.session });
+  } 
+
+  
 }
